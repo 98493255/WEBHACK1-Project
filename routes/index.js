ea7962b32c3c094164fcfd400c2dccd0d19f1cd8
GUNUPURU SURAJ <146669517+98493255@users.noreply.github.com>
2024-03-31 14:11:09 +0530

    Login Work is In Progress

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'FoodForward', loggedIn: req.session.loggedIn });
 });
 
 router.get('/about', function(req, res, next) {
@@ -21,9 +21,26 @@ router.get('/partner', function(req, res, next) {
   res.render('login'); 
 });
 
+router.get('/login', function(req, res, next) {
+  // Here you can add your login validation logic
+  req.session.loggedIn = true; // For demonstration purposes, setting loggedIn to true
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res, next) {
+  req.session.loggedIn = false;
+  res.redirect('/');
+});
+
+router.get('/thankLogin', function(req, res, next) {
+  res.render('thanklogin'); 
+});
+
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' }); 
-});router.get('/donate', function(req, res, next) {
+});
+
+router.get('/donate', function(req, res, next) {
   res.render('donate'); 
 });
 
@@ -31,6 +48,7 @@ router.get('/donate/NGO', function(req, res, next) {
   let restaurant = req.query.restaurant;
   res.render('ngo', { restaurant: restaurant }); 
 });
+
 router.get('/person', function(req, res, next) {
   let name = req.query.name;
   let restaurant = req.query.restaurant;
@@ -42,8 +60,5 @@ router.get('/thankyou', function(req, res, next) {
   let restaurant = req.query.restaurant;
   res.render('thankyou', { name: name, restaurant: restaurant });
 });
-
-
-
 
 module.exports = router;
