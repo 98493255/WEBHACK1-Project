a71e6be8851ce20de36c13ae47c6f2e70166b71e
sathya-reddy1658 <ktr6304@gmail.com>
2024-03-31 12:51:17 +0530

    placeholder changed

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -4,14 +4,20 @@ const btn1 = document.getElementById("btn1");
     const txt2 = document.getElementById("text2");
     const txt3 = document.getElementById("text3"); 
    const head = document.getElementById("head");
+   var reg = document.querySelector(".regg");
+   var org =document.querySelector(".org");
+   
   
     btn1.addEventListener("click", function(event) {
       event.preventDefault();
       btn1.style.backgroundColor = "#0bdd12";
       btn1.style.color="white";
       btn2.style.color="black";
-      head.innerText="logging in as Donor";
+      reg.setAttribute("placeholder", "Enter NGO registration number");
+      org.setAttribute("placeholder", "NGO name (as per records)");
+      head.innerText="logging in as NGO";
       head.style.color="#0bdd12";
+    
       btn2.style.backgroundColor = "";
       txt1.innerText="What NGO gets ";
       txt2.innerText="We help them by providing resources ";
@@ -24,8 +30,10 @@ const btn1 = document.getElementById("btn1");
       event.preventDefault();
       btn1.style.color="black";
       btn2.style.color="white";
-      head.innerText="logging in as NGO";
+      head.innerText="logging in as Donor";
       btn1.style.backgroundColor = "";
+      org.setAttribute("placeholder", "Restaurant name ");
+      reg.setAttribute("placeholder", "Enter restaurant regd number");
       head.style.color="#0bdd12";
       btn2.style.backgroundColor = "#0bdd12";
       txt1.innerText="What Restaurants get";
